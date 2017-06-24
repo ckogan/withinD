73b17264128b7569e21407f3d79e9a8dfdd00278
ckogan <ckogan@trovedata.com>
2017-06-24 16:08:40 -0700

    commented onload function

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,13 +1,13 @@
-.onLoad <- function(libname, pkgname){
-  snapshot_path <-  "./java/withinD/target/withind-1.0-SNAPSHOT.jar"
-  tryCatch({
-    if(file.exists(snapshot_path)){
-      print("Copying primary jar file")
-      file.copy(snapshot_path,'./inst/java/withind-1.0-SNAPSHOT.jar',overwrite=T)
-    } else {
-      stop('Cannot find jar file')
-    }
-  }, error=function(e) {}
-  )
-  .jpackage(pkgname, lib.loc = libname)
-}
+# .onLoad <- function(libname, pkgname){
+#   snapshot_path <-  "./java/withinD/target/withind-1.0-SNAPSHOT.jar"
+#   tryCatch({
+#     if(file.exists(snapshot_path)){
+#       print("Copying primary jar file")
+#       file.copy(snapshot_path,'./inst/java/withind-1.0-SNAPSHOT.jar',overwrite=T)
+#     } else {
+#       stop('Cannot find jar file')
+#     }
+#   }, error=function(e) {}
+#   )
+#   .jpackage(pkgname, lib.loc = libname)
+# }
